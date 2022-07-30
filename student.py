069a330cb52b5e78a014aa395b8be2cbc9230699
garyellow <gary20011110@gmail.com>
2022-07-30 23:26:57 +0800

    adjust some messages

--- student.py
+++ student.py
@@ -403,7 +403,7 @@ def handle_postback(event):
             for item in html.find_all('div', {'class': 'bloglistTitle'}):
                 name = item.find('a').text
                 number = item.find('a').get('href').split('/')[-1]
-                reply_message += name.just(16) + number + '\n'
+                reply_message += name.center(16) + number + '\n'
                 people_cnt += 1
 
         reply_message += '\n' + event.postback.data.split(' ')[0] + '學年度' + department_name[event.postback.data.split(' ')[1]] \
@@ -418,16 +418,16 @@ def handle_postback(event):
 def handle_follow_join(event):
     line_bot_api.reply_message(
         event.reply_token, TextSendMessage(
-            text=
-            '''歡迎使用學號查詢機器人，輸入說明如下：
-                    
-             學號  ->  姓名
-             系名  -> 系代號
-            系代號 ->  系名
-             年分  ->  全系
-            
-            若經過一段時間都沒有回覆
-            可以嘗試再傳一次'''
+            text='''歡迎使用學號查詢機器人
+輸入說明如下：
+
+  學號  ->  姓名
+  系名  -> 系代號
+系代號 ->  系名
+  年分  ->  全系
+
+若經過一段時間都沒有回覆
+可以嘗試再傳一次'''
         )
     )
 
