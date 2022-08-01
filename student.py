1ae7d76760c7036166e0fa80fc80e1edb853f7f9
garyellow <gary20011110@gmail.com>
2022-08-02 02:23:36 +0800

    Delete massage.text on default description

--- student.py
+++ student.py
@@ -90,7 +90,6 @@ def handle_message(event):
                                 PostbackAction(
                                     label='哪次不是',
                                     text='哪次不是',
-                                    inputOption='closeRichMenu',
                                     data='查詢全系' + str(year)
                                 ),
                                 MessageAction(
@@ -111,14 +110,7 @@ def handle_message(event):
 
 @handler.add(PostbackEvent)
 def handle_postback(event):
-    if event.message.text == '使用說明':
-        line_bot_api.reply_message(
-            event.reply_token, TextSendMessage(
-                text='www'
-            )
-        )
-
-    elif event.postback.data == '使用說明':
+    if event.postback.data == '使用說明':
         line_bot_api.reply_message(
             event.reply_token, TextSendMessage(
                 text='輸入學號獲取學生姓名\n輸入系名獲取系代碼\n輸入系代碼獲取系名\n輸入入學學年獲取某系的學生名單\n\n若經過一段時間都沒有回覆\n可以嘗試再傳一次'
